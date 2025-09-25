a85f69d5110b4b825786a02b63660654852156a2
Luis <luisito58517025851702@hotmail.com>
2025-09-26 05:18:00 +0800

    Primer Commit

---
+++ src/main/java/com/example/Data_JPA_API_SEM3/DataJpaApiSem3Application.java
@@ -0,0 +1,13 @@
+package com.example.Data_JPA_API_SEM3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataJpaApiSem3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataJpaApiSem3Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Data_JPA_API_SEM3/DataJpaApiSem3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Data_JPA_API_SEM3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataJpaApiSem3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
